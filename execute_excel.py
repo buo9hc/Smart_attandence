65d20431d956e2025806d679eb42b72d02198f11
HoangBuiQuoc <hbui9701@gmail.com>
2023-11-10 21:08:30 +0700

    Generate file text which contain data from execel Use for insert data to table

--- execute_excel.py
+++ execute_excel.py
@@ -1,13 +1,17 @@
+print("test")
 
 import openpyxl
-import unidecode
-print("test")
-import mysql.connector
+
+rmsign_table = str.maketrans(
+    "ÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬĐÈÉẺẼẸÊẾỀỂỄỆÍÌỈĨỊÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢÚÙỦŨỤƯỨỪỬỮỰÝỲỶỸỴáàảãạăắằẳẵặâấầẩẫậđèéẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵ",
+    "A"*17 + "D" + "E"*11 + "I"*5 + "O"*17 + "U"*11 + "Y"*5 + "a"*17 + "d" + "e"*11 + "i"*5 + "o"*17 + "u"*11 + "y"*5
+)
+
 import sys
-path = sys.argv[1]
-# path = "uploads/DSDiemDanh_222SCDA331629_02CLC.xlsx"
-table_name = sys.argv[2]
-# table_name = "SCDA331629"
+# path = sys.argv[1]
+path = "uploads/DSDiemDanh_222SCDA331629_02CLC.xlsx"
+# table_name = sys.argv[2]
+table_name = "SCDA331629"
 
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
@@ -17,40 +21,37 @@ column = sheet_obj.max_column
 
 #//Find Row contain table header
 table = []
+temp = 0
 for i in range(1, row + 1):  
     cell_obj = sheet_obj.cell(row = i, column = 1)  
-    # print(cell_obj.value)
     if(cell_obj.value == "STT" or cell_obj.value == "TT"):
+        temp = i
         for j in range(1,column + 1):
             table_header = sheet_obj.cell(row = i, column = j)
             if (table_header.value != None):
                 rm_sign = table_header.value
-                rm_sign = unidecode.unidecode(rm_sign)
+                rm_sign = rm_sign.translate(rmsign_table)
                 rm_sign = rm_sign.replace(" ", "_")
                 table.append(rm_sign)
                 # print(str(table_header.value))
-        break
+    if (temp!=0 and i > temp):
+        for j in range(2,column + 1):
+            cell_data = sheet_obj.cell(row = i, column = j)
+            if (cell_data.value != None):
+                f = open("data.txt", "a")
+                f.write(str(cell_data.value))
+                f.close()
+        f = open("data.txt", "a")
+        f.write("\n")
+        f.close()
+# f = open("creatable_script.txt", "x")
 
+# f.write("CREATE TABLE " + table_name + "(" + table[0] + " MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
+#                                          + table[1] + " VARCHAR(8) NOT NULL, "
+#                                          + table[2] + " VARCHAR(30) NOT NULL, "
+#                                          + table[3] + " VARCHAR(10) NOT NULL, "
+#                                          + table[4] + " VARCHAR(15) NOT NULL, "
+#                                          + table[5] + " VARCHAR(15) NOT NULL"
+#                                          + ")")
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="admin",
-  password="123456",
-  database="DanhSachLop"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE TABLE " + table_name + "(" + table[0] + " MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, "
-                                         + table[1] + " VARCHAR(8) NOT NULL, "
-                                         + table[2] + " VARCHAR(30) NOT NULL, "
-                                         + table[3] + " VARCHAR(10) NOT NULL, "
-                                         + table[4] + " VARCHAR(15) NOT NULL, "
-                                         + table[5] + " VARCHAR(15) NOT NULL"
-                                         + ")")
-
-
-# mycursor.execute("SHOW TABLES")
-
-# for x in mycursor:
-#   print(x)
+# f.close()
--- upload.php
+++ upload.php
@@ -8,12 +8,38 @@
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
-  $python = shell_exec("which python");
-  $command = escapeshellcmd('$python execute_excel.py '.$target_file." ".$class_name);
+
+  $command = escapeshellcmd('python execute_excel.py '.$target_file." ".$class_name);
   $out = shell_exec($command);
 
-  echo ($out);
+  $myfile = fopen("creatable_script.txt", "r") or die("Unable to open file!");
+  $creatable_script = fread($myfile,filesize("creatable_script.txt"));
+  fclose($myfile);
+  
+  // echo ($creatable_script);
 
+  $servername = "localhost";
+  $username = "admin";
+  $password = "123456";
+  $dbname = "DanhSachLop";
 
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } 
 
+  $sql = $creatable_script;
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Table $class_name created successfully";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+  $conn->query("drop table $class_name");  // use for debug
+
+  $conn->close();
+
+  shell_exec("rm -rf creatable_script.txt");
 ?>
